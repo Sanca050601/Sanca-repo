4885917ea7b270e135df9fca6e8cc3ecb9999564
Sanca050601 <90603494+Sanca050601@users.noreply.github.com>
2021-10-03 10:45:30 +0200

    Update

--- python.py
+++ python.py
@@ -1,1 +1,16 @@
-print('Hello')
+num_1 = '7'
+num_2 = '8'
+
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+print(num_1 + num_2)
+
+
+num_1 = '7.5'
+num_2 = '2.5'
+
+num_1 = float(num_1)
+num_2 = float(num_2)
+
+print(num_1 + num_2)
